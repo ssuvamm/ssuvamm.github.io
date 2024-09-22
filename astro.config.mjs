71259be9706a401aea58b0b5f43c9a80c7857819
ssuvamm <ssuvamm@gmail.com>
2024-09-22 13:08:49 +0530

    asrro

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+// import vercel from "@astrojs/vercel/serverless";
+
+// If you are not going to use vercel analytics you can delete the analytics
+// If you want to remove vercel from your project you can do npm uninstall vercel
+export default defineConfig({
+  site: "https://ssuvamm.github.io",
+  base: "ssuvamm",
+});
+
+//If you don't have vercel the configuration will be empty
+
+//ej : export default defineConfig({});
---
+++ src/const/const.js
@@ -0,0 +1,214 @@
+export const studies = [
+  {
+    title: "B.Tech",
+    corporation: "Adamas Institute of Technology",
+    date: "2012 - 2016",
+  },
+  {
+    title: "Full Stack Web Development(M.E.R.N)",
+    corporation: "Vidyastu",
+    date: "Aug 2023 - Dec 2023",
+  },
+  {
+    title: "Python Programming For Beginners In Data Science",
+    corporation: "udemy",
+    date: "Jan 2022 - Sep 2022",
+  },
+];
+
+export const experience = [
+  {
+    title: "Web Development",
+    corporation: "Escaliers Business Solutions",
+    date: "Jul 2024 - Present",
+  },
+  {
+    title: "Web Development",
+    corporation: "Crush Code Works",
+    date: "Mar 2024 - Jun 2024",
+  },
+  {
+    title: "Full Stack Development",
+    corporation: "Sisyphus Infotech Private Limited",
+    date: "Jan 2024 - Mar 2024",
+  },
+  {
+    title: "Human Resources (HR)",
+    corporation: "Amazon",
+    date: "Jul 2022 - Mar 2023",
+  },
+  {
+    title: "Customer Service",
+    corporation: "Amazon",
+    date: "Sep 2021 - Dec 2021",
+  },
+  {
+    title: "Electrical Engineering",
+    corporation: "Nyntic Lab Private Limited",
+    date: "Aug 2017 - Sep 2018",
+  },
+  {
+    title: "Freelance",
+    corporation: "Appen",
+    date: "Jul 2017 - Sep 2021",
+  },
+  {
+    title: "Engineering",
+    corporation: "Rajdeep Info Techno Pvt. Ltd",
+    date: "Dec 2016 - Jun 2017",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  adonisjs: "adonisjs",
+  bootstrap: "bootstrap",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Radix UI",
+    icon: IconKeys.radix,
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite,
+  },
+  {
+    title: "Shadcn UI",
+    icon: IconKeys.shadcn,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+  {
+    title: "Bootstrap",
+    icon: IconKeys.bootstrap,
+  },
+  {
+    title: "Adonis Js",
+    icon: IconKeys.adonisjs,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/woodrockgroup.webp",
+    title: "Woodrock Group - Company Website",
+    link: "https://woodrockgroup.in",
+    description: `Plain Company Website built on ReactJS and TailwindCSS.`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/jobrator.webp",
+    title: "Jobrator - Find Jobs",
+    link: "https://human-capital-web.vercel.app/",
+    description: `Jobrator - A web application that helps you find and apply for jobs.`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.github,
+      },
+      {
+        icon: IconKeys.bootstrap,
+      },
+      {
+        icon: IconKeys.nodejs,
+      },
+      {
+        icon: IconKeys.bootstrap,
+      },
+      {
+        icon: IconKeys.adonisjs,
+      },
+    ],
+  },
+];
