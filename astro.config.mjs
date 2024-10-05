4a62e9f96e2e180db22b45c612a4042731c3da7f
ssuvamm <ssuvamm@gmail.com>
2024-10-05 15:40:06 +0530

    Feat: Company dashboard - manage jobs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  site: "https://ssuvamm.github.io", 
+  site: "https://ssuvamm.github.io",
   site: "https://suvam.suvamsinha.us.kg",
   // base: "ssuvamm",
+  devToolbar: {
+    enabled: false,
+  },
 });
 
 //If you don't have vercel the configuration will be empty
